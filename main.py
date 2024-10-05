14d026b0e7d1a69a38ebee7a331a7e243883fb62
yasminebosle <fareedahusman@ryanacademy.shc.ng>
2024-10-05 10:30:52 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+print("Enter Marks Obtained in 4 Subject: ")
+math = int(input("maths :"))
+english = int(input("english :"))
+science = int(input("science :"))
+hindi = int(input("hindi :"))
+
+
+sum = math+english+science+hindi
+print(sum)
+
+perc = (sum/400)*100
+
+print (perc)
